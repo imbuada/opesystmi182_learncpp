cfeeee3a31cfd60a8ec17d6d4a4b6a8710c16934
imbuada <imbuada@student.apc.edu.ph>
2019-11-28 02:43:20 +0000

    initial commit for learncpp02

---
+++ learncpp02.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int userInput; //variable for user's input
+	
+	cout << "What Element do you control?"<< endl;
+	
+	//ask for user input
+	cout <<"Enter a number from 1 -4: ";
+	cin userInput;
+
+	
+	if(userInput == 1){
+		cout << "Your element is FIRE" << endl;
+	} 
+	else if (userInput == 2){
+		cout << "Your element is WATER" << endl;
+	}
+	else if (userInput == 3) {
+		cout << "Your element is EARTH" << endl;
+	}
+	else if (userInput == 4){
+		cout << "Your element is AIR" << endl;
+	}
+
+
+
+
+
+}
