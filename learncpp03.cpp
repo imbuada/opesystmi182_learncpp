92eddc96a7ecf2048682621844e42e3e73177b14
imbuada <imbuada@student.apc.edu.ph>
2019-11-28 02:57:12 +0000

    initial commit for learncpp03

---
+++ learncpp03.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int userInput, rng;
+
+	cout << "Number Guessing Game!" << endl;
+
+	//prompting for user input
+	cout << "Enter a number from 1 to 10: ";
+	cin >> userInput;
+
+	return 0;
+
+}
