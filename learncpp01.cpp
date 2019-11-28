ab9160d375ecaba75d25e78ee9c9a4c8504fa2a5
imbuada <imbuada@student.apc.edu.ph>
2019-11-28 02:28:21 +0000

    modified learncpp02

--- learncpp01.cpp
+++ learncpp01.cpp
@@ -2,11 +2,25 @@
 using namespace std;
 
 int main(){
+	double userInput; //variable for user's input
+	double perimeter, area;
+
 	cout << "Square Calculator" << endl;
+	
+
+	cout << "Enter the length of the square: ";
+	cin >> userInput; //get user input
+
+
+
+	//Compute for area and perimeter
+	perimeter = 4 * userInput;
+	area = userInput * userInput; 
+
+	//Print output
+	cout << "Perimeter: " << perimeter << endl;
+	cout << "Area: " << area << endl;
+
 	return 0;
-
-
-
-
 
 }
