d936217b8a71a245132bea4113f3ff10f8f27712
imbuada <imbuada@student.apc.edu.ph>
2019-11-28 03:09:35 +0000

    modified learncpp03

--- learncpp03.cpp
+++ learncpp03.cpp
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 #include <stdlib.h>
 
 int main(){
@@ -9,6 +10,23 @@ int main(){
 	//prompting for user input
 	cout << "Enter a number from 1 to 10: ";
 	cin >> userInput;
+
+	rng = rand() % 10 + 1;
+
+	while (userInput != rng){
+		if (userInput >rng){
+			cout << "GO LOWER!";
+			cin >> userInput;
+		}
+		else if (userInput < rng){
+			cout << "GO HIGHER";
+			cin >> userInput;
+		}
+	}
+	
+	cout << "Congratulations! The number is " << rng << endl;
+
+
 
 	return 0;
 
